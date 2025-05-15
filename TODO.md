e524e6dde9620a611db453c7e77d3705e7d3c4bc
Sargates <nick@TERRA-NG>
2025-05-15 09:33:25 -0500

    Initial commit

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# Things to do for setup
+- [ ] Proper git integration
+- [ ] LSP
+- [ ] nvim config through SSH
+    - Look into this
---
+++ lua/plugins/comment.lua
@@ -0,0 +1,8 @@
+return {
+    'numToStr/Comment.nvim',
+    opts = {
+        padding = true,
+        -- toggler = { line = {'gcc', '^C'}, block = 'gbc' },
+        -- add any options here
+    }
+}
