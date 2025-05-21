bfaba31b1c5c3d733711e7a9f3f7602b62b85e78
Sargates <nick@TERRA-NG>
2025-05-21 14:58:35 -0500

    Added telescope configuration

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,38 @@ return {
 		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
 	},
 	config = function()
-		vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files)
+		require('telescope').setup {
+			defaults = {
+				vimgrep_arugments = '--smartcase'
+			},
+			extensions = {
+				file_browser = {
+					grouped = true,
+					theme = "gruvbox",
+					-- disables netrw and use telescope-file-browser in its place
+					hijack_netrw = true,
+					mappings = {
+						["i"] = {
+							-- your custom insert mode mappings
+						},
+						["n"] = {
+							-- your custom normal mode mappings
+						},
+					},
+				}
+			}
+			-- pickers = {
+			-- 	find_files = {
+			-- 		hidden = true
+			-- 	}
+			-- }
+		}
+		vim.keymap.set("n", "<space>fd",
+			function()                        -- https://github.com/creativenull/dotfiles/blob/4fc5971029604ff1c338cfe0c6c2c333d9ee3ec4/.config/nvim-nightly/lua/creativenull/plugins/config/telescope.lua#L17
+				require("telescope.builtin").find_files {
+					find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' }
+				}
+			end)
 		vim.keymap.set("n", "<space>en", function()
 			require('telescope.builtin').find_files {
 				cwd = vim.fn.stdpath("config")
