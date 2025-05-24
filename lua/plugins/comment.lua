cf0012e406c32107c59bf867adeb1114ca178548
Sargates <nick@TERRA-NG>
2025-05-24 17:20:04 -0500

    Added commenting config. Better comments doesn't work right now.

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,8 +1,17 @@
 return {
-    'numToStr/Comment.nvim',
-    opts = {
-        padding = true,
-        -- toggler = { line = {'gcc', '^C'}, block = 'gbc' },
-        -- add any options here
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            padding = true,
+            -- toggler = { line = {'gcc', '^C'}, block = 'gbc' },
+            -- add any options here
+        }
+    },
+
+    {
+        "jbgutierrez/vim-better-comments",
+        branch = "master",
+        config = function() end,
+        -- event = "User AstroFile",
     }
 }
