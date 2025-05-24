e371c179392c22ac210510c1a3082ae706446990
Sargates <nick@TERRA-NG>
2025-05-24 17:46:07 -0500

    Add `url-open.lua` -- Configured for WSL for now

---
+++ lua/plugins/url-open.lua
@@ -0,0 +1,28 @@
+return {
+    -- lazy.nvim
+    {
+        "sontungexpt/url-open",
+        event = "VeryLazy",
+        cmd = "URLOpenUnderCursor",
+        config = function()
+            local status_ok, url_open = pcall(require, "url-open")
+            if not status_ok then
+                return
+            end
+            url_open.setup ({
+                open_app = "wslview",
+                open_only_when_cursor_on_url = true,
+                highlight_url = {
+                    all_urls = {
+                        enabled = false
+                    },
+                    cursor_move = {
+                        enabled = false
+                    }
+                }
+            })
+            vim.keymap.set("n", "gx", "<esc>:URLOpenUnderCursor<cr>")
+        end,
+        -- opts = 
+    },
+}
