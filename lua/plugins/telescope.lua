8f334b4e9db09fbc60b72143b71cb4074ab487fd
Sargates <nick@TERRA-NG>
2025-05-24 17:35:50 -0500

    Update telescope configuration. Remove `project.lua` and incorporate into `telescope.lua`

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,47 +1,81 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.8',
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
-	},
-	config = function()
-		require('telescope').setup {
-			defaults = {
-				vimgrep_arugments = '--smartcase'
-			},
-			extensions = {
-				file_browser = {
-					grouped = true,
-					theme = "gruvbox",
-					-- disables netrw and use telescope-file-browser in its place
-					hijack_netrw = true,
-					mappings = {
-						["i"] = {
-							-- your custom insert mode mappings
-						},
-						["n"] = {
-							-- your custom normal mode mappings
-						},
-					},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
+		},
+		-- opts = {
+		-- 	defaults = {
+		-- 		-- `--smartcase` is to disable case sensitivity
+		-- 		-- `--color=never` is recommended by `:h telescope.defaults.vimgrep_arguments`
+		-- 		-- See here for the rest: https://github.com/BurntSushi/ripgrep/issues/623#issuecomment-659909044
+		-- 	},
+		-- 	extensions = {
+		-- 		-- file_browser = {
+		-- 		-- 	grouped = true,
+		-- 		-- 	theme = "gruvbox",
+		-- 		-- 	-- disables netrw and use telescope-file-browser in its place
+		-- 		-- 	hijack_netrw = true,
+		-- 		-- 	mappings = {
+		-- 		-- 		["i"] = {
+		-- 		-- 			-- your custom insert mode mappings
+		-- 		-- 		},
+		-- 		-- 		["n"] = {
+		-- 		-- 			-- your custom normal mode mappings
+		-- 		-- 		},
+		-- 		-- 	},
+		-- 		-- }
+		-- 	}
+		-- },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					vimgrep_arguments = {
+						"rg",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+						"--hidden",
+						"--glob",
+						"!.git",
+					}
 				}
-			}
-			-- pickers = {
-			-- 	find_files = {
-			-- 		hidden = true
-			-- 	}
-			-- }
-		}
-		vim.keymap.set("n", "<space>fd",
-			function()                        -- https://github.com/creativenull/dotfiles/blob/4fc5971029604ff1c338cfe0c6c2c333d9ee3ec4/.config/nvim-nightly/lua/creativenull/plugins/config/telescope.lua#L17
+			})
+			vim.keymap.set("n", "<space>en", function()
 				require("telescope.builtin").find_files {
-					find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' }
+					cwd = vim.fn.stdpath("config")
 				}
 			end)
-		vim.keymap.set("n", "<space>en", function()
-			require('telescope.builtin').find_files {
-				cwd = vim.fn.stdpath("config")
-			}
-		end)
-	end
+		end
+	},
+	-- {
+	-- 	"nvim-telescope/telescope-file-browser.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-telescope/telescope.nvim",
+	-- 		"nvim-lua/plenary.nvim"
+	-- 	},
+	-- 	config = function ()
+	-- 		require("telescope").load_extension("file_browser")
+	-- 	end
+	-- },
+	{
+		"nvim-telescope/telescope-project.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function ()
+			require("telescope").load_extension("project")
+			vim.api.nvim_set_keymap(
+				"n",
+				"<C-p>",
+				":lua require('telescope').extensions.project.project{}<CR>",
+				{noremap = true, silent = true}
+			)
+		end
+	},
 }
