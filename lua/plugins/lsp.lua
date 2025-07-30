990f8430ed0f4e98d471bafa1fbb08be1782756a
Sargates <nick@TERRA-NG>
2025-07-30 17:30:32 -0500

    Updated LSP config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,11 +6,32 @@ return {
             "SmiteshP/nvim-navbuddy"
             -- "hrsh7th/cmp-nvim-lsp"
         },
+        config = function()
+            local lspconfig = require("lspconfig")
+
+            lspconfig.bashls.setup({
+                filetypes = { "sh", "bash", "zsh" },
+                settings = {
+                    bashIde = {
+                        globPattern = "**/*@(.sh|.bash|.zsh)",
+                    }
+                }
+            })
+            -- lspconfig.omnisharp.setup({
+            --     enable_import_completion = true,
+            --     enable_decompilation_support = true
+            -- })
+        end
     },
     {
         "mason-org/mason.nvim",
         config = function()
-            require('mason').setup {}
+            require('mason').setup({
+                registries = {
+                    "github:mason-org/mason-registry",
+                    "github:Crashdummyy/mason-registry",
+                },
+            })
         end
     },
     {
@@ -19,7 +40,19 @@ return {
             "mason-org/mason.nvim",
         },
         config = function()
-            require('mason-lspconfig').setup {}
+            require('mason-lspconfig').setup {
+                ensure_installed = {
+                    "bashls"
+                    -- "ast-grep",
+                    -- "rust-analyzer",
+                    -- "asm-lsp",
+                    -- "bash-language-server",
+                    -- "clangd",
+                    -- "jq",
+                    -- "ts_ls"
+                },
+                automatic_enable = true
+            }
             -- require("lspconfig").clangd.setup({
             --     on_attach = function(client, bufnr)
             --         require("nvim-navbuddy").attach(client, bufnr)
@@ -51,6 +84,44 @@ return {
             }
         }
     },
+    {
+        "seblyng/roslyn.nvim",
+        ft = "cs",
+        ---@module 'roslyn.config'
+        ---@type RoslynNvimConfig
+        config = function()
+            require("roslyn").setup {
+                filewatching = "roslyn"
+            }
+
+            vim.lsp.config("roslyn", {
+                on_attach = function() end,
+                settings = {
+                    ["csharp|inlay_hints"] = {
+                        -- csharp_enable_inlay_hints_for_implicit_object_creation = true,
+                        -- csharp_enable_inlay_hints_for_implicit_variable_types = true,
+                        -- csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+                        -- csharp_enable_inlay_hints_for_types = true,
+                        -- dotnet_enable_inlay_hints_for_indexer_parameters = true,
+                        -- dotnet_enable_inlay_hints_for_literal_parameters = true,
+                        -- dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+                        -- dotnet_enable_inlay_hints_for_other_parameters = true,
+                        -- dotnet_enable_inlay_hints_for_parameters = true,
+                        -- dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+                        -- dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+                        -- dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+                    },
+                    ["csharp|code_lens"] = {
+                        dotnet_enable_references_code_lens = true,
+                    },
+                    ["csharp|completion"] = {
+                        dotnet_show_completion_items_from_unimported_namespaces = true,
+                        dotnet_show_name_completion_suggestions = true,
+                    },
+                },
+            })
+        end
+    }
     -- {
     --     "SmiteshP/nvim-navbuddy",
     --     dependencies = {
