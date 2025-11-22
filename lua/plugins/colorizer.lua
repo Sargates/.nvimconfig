e2f72ebbdb2a65726f281e9c43527884e6ab3313
Sargates <78454343+Sargates@users.noreply.github.com>
2025-11-21 21:37:29 -0600

    Add rainbow delimiters plugin

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,6 +1,36 @@
 return {
-    "catgoose/nvim-colorizer.lua",
-    event = "BufReadPre",
-    opts = { -- set to setup table
+    {
+        "catgoose/nvim-colorizer.lua",
+        event = "BufReadPre",
+        opts = { -- set to setup table
+        },
     },
+    {
+        "hiphish/rainbow-delimiters.nvim",
+        config = function()
+            require('rainbow-delimiters.setup').setup {
+                strategy = {
+                    [''] = 'rainbow-delimiters.strategy.global',
+                    -- vim = 'rainbow-delimiters.strategy.local',
+                },
+                query = {
+                    [''] = 'rainbow-delimiters',
+                    -- lua = 'rainbow-blocks',
+                },
+                priority = {
+                    [''] = 110,
+                    -- lua = 210,
+                },
+                highlight = {
+                    -- 'RainbowDelimiterRed',
+                    'RainbowDelimiterYellow',
+                    -- 'RainbowDelimiterBlue',
+                    -- 'RainbowDelimiterOrange',
+                    -- 'RainbowDelimiterGreen',
+                    'RainbowDelimiterViolet',
+                    'RainbowDelimiterCyan',
+                }
+            }
+        end
+    }
 }
