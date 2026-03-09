43ee226af31891cf3fb0fcae91f8268c9d8cee38
Sargates <78454343+Sargates@users.noreply.github.com>
2026-03-09 12:57:32 -0500

    Move `colorschemes.lua`. Add `lualine.nvim`

--- lua/plugins/ui/display.lua
+++ lua/plugins/ui/display.lua
@@ -42,5 +42,22 @@ return {
                 light = "lotus"
             },
         }
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+        config = function()
+            -- Source: https://github.com/jdah/dotfiles/blob/2b984059a68637640f03732569e24e317e7c9115/.config/nvim/init.lua#L61-L62
+            require('lualine').setup{
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_c = { 'filename' },
+                    lualine_x = {'encoding', 'fileformat', 'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
+                },
+            }
+        end
     }
 }
