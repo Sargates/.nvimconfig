79de4e4a9371dde773e359f6e857d7fb2fdecaaf
Sargates <78454343+Sargates@users.noreply.github.com>
2026-03-19 22:34:48 -0500

    Re-add vimtex plugin

--- lua/plugins/third-party/doc-preview.lua
+++ lua/plugins/third-party/doc-preview.lua
@@ -8,6 +8,16 @@ return {
         end,
         ft = { "markdown" },
     },
+    {
+        "lervag/vimtex",
+        lazy = false,     -- we don't want to lazy load VimTeX
+        tag = "v2.17", -- uncomment to pin to a specific release
+        init = function()
+            -- VimTeX configuration goes here, e.g.
+            vim.g.vimtex_view_method = "zathura"
+            -- vim.g.vimtex_compiler_method = "make"
+        end
+    }
     -- {
     --     'adelarsq/image_preview.nvim',
     --     event = 'VeryLazy',
