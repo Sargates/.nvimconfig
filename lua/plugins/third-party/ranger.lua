32f68b6009c863c1e023bf0ebb162b7991eae8cc
Sargates <78454343+Sargates@users.noreply.github.com>
2026-03-29 16:03:27 -0500

    Remove `ranger.lua` and replace with `yazi.lua`

--- lua/plugins/third-party/ranger.lua
+++ lua/plugins/third-party/ranger.lua
@@ -1,25 +1,25 @@
 return {
-    "kelly-lin/ranger.nvim",
-    config = function()
-        require("ranger-nvim").setup({
-            replace_netrw = true,
-            ui = {
-                border = "none",
-                width = 1,
-                height = 0.96,
-                x = 0.5,
-                y = 0.0,
-            },
-            enable_cmds = false,
-            keybinds = {},
-        })
-        vim.api.nvim_set_keymap("n", "<leader>ef", "", {
-            noremap = true,
-            callback = function()
-                require("ranger-nvim").open(true)
-            end,
-            desc = "Open Ranger"
-        })
-    end,
-    -- opts = 
+    -- "kelly-lin/ranger.nvim",
+    -- config = function()
+    --     require("ranger-nvim").setup({
+    --         replace_netrw = true,
+    --         ui = {
+    --             border = "none",
+    --             width = 1,
+    --             height = 0.96,
+    --             x = 0.5,
+    --             y = 0.0,
+    --         },
+    --         enable_cmds = false,
+    --         keybinds = {},
+    --     })
+    --     vim.api.nvim_set_keymap("n", "<leader>ef", "", {
+    --         noremap = true,
+    --         callback = function()
+    --             require("ranger-nvim").open(true)
+    --         end,
+    --         desc = "Open Ranger"
+    --     })
+    -- end,
+    -- -- opts = 
 }
