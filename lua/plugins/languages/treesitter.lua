fa7e06ad4e63f964b04db44265654bf3291412af
Sargates <78454343+Sargates@users.noreply.github.com>
2026-03-28 23:50:02 -0500

    Add `tree-sitter` incremental selection. Will be obsolete in Neovim 0.12.0

--- lua/plugins/languages/treesitter.lua
+++ lua/plugins/languages/treesitter.lua
@@ -55,6 +55,21 @@ return {
         end
     },
     {
+        "MeanderingProgrammer/treesitter-modules.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = {
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<A-o>",
+                    node_incremental = "<A-o>",
+                    scope_incremental = "<A-O>",
+                    node_decremental = "<A-i>",
+                },
+            },
+        },
+    },
+    {
         "nvim-treesitter/nvim-treesitter-textobjects",
         branch = "main",
         init = function()
